12f8f1d0bc11a07ab514551daa35dff47744e8e2
nagaeyuki <yuki.0123.n@gmail.com>
2018-12-10 10:27:36 +0900

    test

--- git.sh
+++ git.sh
@@ -1,9 +1,5 @@
- git add .
+git add .
 
-  # commit
-  if [ -z "$1" ]; then
-      read -p "コミットメッセージを入力: " commitMessage
-      git commit -m "$commitMessage"
-  else
-      git commit -m "$1"      
-  fi
+read -p "コミットメッセージを入力: " commitMessage
+git commit -m "$commitMessage"
+  
